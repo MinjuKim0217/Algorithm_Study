2ba10590ce69c2676263df38a564bbbf10b1aa17
Minju Kim <76491201+MinjuKim0217@users.noreply.github.com>
2022-07-13 18:57:54 +0900

    Create 704. Binary Search.py

---
+++ LeetCode/704. Binary Search.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        for i in nums:
+            if i == target:
+                return nums.index(i)
+            else:
+                continue
+        
+        return -1
+        
